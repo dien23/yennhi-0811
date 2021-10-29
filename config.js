36b2adc174b014da11d8b70eef526b55aa6179aa
Vo Thanh Dien <vothanhdien23520@gmail.com>
2021-10-28 20:43:18 -0700

    Upload sinh nhat Yen Nhi

---
+++ config.js
@@ -0,0 +1,15 @@
+const nameGirl = "🙊Yến Nhi🙊";
+const giftUrl = "http://nodemy.vn";
+const eventName = "️🎊Happy Birthday️🎊";
+const titleCard = "Chúc bạn có ngày sinh nhật thật vui";
+const contentCard =
+  "Happy Birthdayyy!!!!! Mình hông biết mình có phải là người đầu tiên chúc sinh nhật bạn hông nữa. Nhưng mà sao bao chuyện thì vẫn mong bạn luôn vui vẻ, hạnh phúc, lạc quan...Mong bạn luôn thành công và sẽ ra trường thật sớm. Dù sao thì chúc bạn có ngày sinh nhật thật vui nhaaa!!!️🎊️🎉️🎈️🎷🎂.Luôn cười như mấy tấm hình này nha. Xin lỗi vì t thiệp và mấy lời sến súa này =]]]";
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = "hot-girl.png";
+const giftImage2 = "hot-girl2.png";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
