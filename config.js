cfef413c29ce1c14d9bb3ba3bf33783497442aee
Vo Thanh Dien <vothanhdien23520@gmail.com>
2021-10-29 07:02:25 -0700

    Upload

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const nameGirl = "🙊Yến Nhi🙊";
 const giftUrl = "http://nodemy.vn";
 const eventName = "️🎊Happy Birthday️🎊";
-const titleCard = "Chúc bạn có ngày sinh nhật thật vui";
+const titleCard = "Chúc bạn có ngày sinh nhật thật vui nhaaa";
 const contentCard =
-  "Happy Birthdayyy!!!!! Mình hông biết mình có phải là người đầu tiên chúc sinh nhật bạn hông nữa. Nhưng mà sao bao chuyện thì vẫn mong bạn luôn vui vẻ, hạnh phúc, lạc quan...Mong bạn luôn thành công và sẽ ra trường thật sớm. Dù sao thì chúc bạn có ngày sinh nhật thật vui nhaaa!!!️🎊️🎉️🎈️🎷🎂.Luôn cười như mấy tấm hình này nha. Xin lỗi vì t thiệp và mấy lời sến súa này =]]]";
+  "Happy Birthdayyy!!!!! Mình hông biết mình có phải là người đầu tiên chúc sinh nhật bạn hông nữa. Nhưng mà sao bao chuyện thì vẫn mong bạn luôn vui vẻ, hạnh phúc, lạc quan...Mong bạn luôn thành công và sẽ ra trường thật sớm. Dù sao thì chúc bạn có ngày sinh nhật thật vui nhaaa!!!️🎊️🎉️🎈️🎷🎂.Luôn cười như mấy tấm hình này nha. Xin lỗi vì tấm thiệp và mấy lời sến súa này =]]]";
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ:
